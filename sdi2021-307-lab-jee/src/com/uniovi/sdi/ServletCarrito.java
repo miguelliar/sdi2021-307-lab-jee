75505e0e1a0c6f39af2f8f7ffd5cf9eb8b0f47d2
UO270927 <uo270927@uniovi.es>
2021-02-01 20:09:57 +0100

    SDI-2021-307-1.8-MVC, Modelo Vista Controlador.

--- sdi2021-307-lab-jee/src/com/uniovi/sdi/ServletCarrito.java
+++ sdi2021-307-lab-jee/src/com/uniovi/sdi/ServletCarrito.java
@@ -34,30 +34,25 @@ public class ServletCarrito extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		
 		HttpSession session=request.getSession();
-		ConcurrentHashMap<String,Integer> carrito = (ConcurrentHashMap<String,Integer>) request.getSession().getAttribute("carrito");
-		
+		HashMap<String,Integer> carrito = 
+				(HashMap<String,Integer>) request.getSession().getAttribute("carrito");
 		// No hay carrito, creamos uno y lo insertamos en sesión
 		if (carrito == null) {
-			carrito = new ConcurrentHashMap<String,Integer>();
+			carrito = new HashMap<String,Integer>();
 			request.getSession().setAttribute("carrito", carrito);
 		}
 		
 		String producto = request.getParameter("producto");
-		if (producto != null){
+		if ( producto != null){
 			insertarEnCarrito(carrito, producto);
 		}
 		
-		response.setCharacterEncoding("UTF-8");
-		response.setContentType("text/html");
-		PrintWriter out = response.getWriter();
-		out.println("<HTML>");
-		out.println("<HEAD><TITLE>Tienda SDI: carrito</TITLE></HEAD>");
-		out.println("<BODY>");
-		out.println(carritoEnHTML(carrito)+"<br>");
-		out.println("<a href=\"index.jsp\">Volver</a></BODY></HTML>");
+		// Retornar la vista con parámetro "carrito"
+		request.setAttribute("paresCarrito", carrito);
+		getServletContext().getRequestDispatcher("/vista-carrito.jsp").forward(request, response);
 	}
 	
-	private void insertarEnCarrito(ConcurrentHashMap<String, Integer> carrito, String claveProducto) {
+	private void insertarEnCarrito(HashMap<String, Integer> carrito, String claveProducto) {
 		if (carrito.get(claveProducto) == null) {
 			carrito.put(claveProducto, new Integer(1));
 		} else  {
@@ -65,14 +60,7 @@ public class ServletCarrito extends HttpServlet {
 			carrito.put(claveProducto, new Integer(numeroArticulos) + 1);
 		}
 	}
-	
-	private String carritoEnHTML(Map<String, Integer> carrito) {
-		String carritoEnHTML = "";
-		
-		for (String key:carrito.keySet())
-			carritoEnHTML+="<p>["+key+"], "+carrito.get(key)+" unidades</p>";
-		return carritoEnHTML;
-	}
+
 
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
